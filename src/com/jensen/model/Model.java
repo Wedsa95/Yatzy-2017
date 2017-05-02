e92974ea4d7766fc97a8417d86550cbc5faead1c
yoshi175 <takeyoshi_92@hotmail.com>
2017-05-02 20:19:43 +0200

    javadoc done

--- src/com/jensen/model/Model.java
+++ src/com/jensen/model/Model.java
@@ -1,43 +1,43 @@
 package com.jensen.model;
 
+/**
+ * 
+ * @author Takeyoshi, Jonas, Sergej, Oskar
+ * @version 1.0
+ */
 public class Model {
 	
 	private String[] scoreName =
-	{"Spelare", "Ettor" ,"Tvåer",
-	"Treor", "Fyror" ,"Femor",
+	{"Spelare", "Ettor" ,"TvÃ¥er",
+	"Treor", "Fyror" ,"Femmor",
 	"Sexor","Summa","Bonus", "Par", 
-	"Två Par","Tre Tal", "Fyr Tal", 
-	"Liten","Stor", "Fullt Hus", 
-	"Chans","YATZY", "Total"}; 
+	"TvÃ¥ Par","Tretal", "Fyrtal", 
+	"Liten Stege","Stor Stege", "KÃ¥k", 
+	"Chans","YATZY", "Totalt"}; 
 	
 	private String[] playerName;
 	private static int numberPlayers;
 	private Dice[] diceArray = new Dice[5];
-	//TaBort
 	private String[][] scoreBoard;
-	
-	//yoshi*
-	private Dice dice; // behövs?
-	
-	public int[][] underlyingScoreboard = new int[19][6];
-	public boolean[][] placedOrNot = new boolean[19][6];
-	public int booleanCounter;
-	public boolean continuePlaying = true;
-	public boolean placementDone = true;
-	public boolean resetDiceResult = false;
-	public boolean onePlayerRule;
-	public int gameCompleted = 0;
-	public static int rollCounter;
+	private int[][] underlyingScoreboard = new int[19][6];
+	private boolean[][] placedOrNot = new boolean[19][6];
+	private boolean diceThrow[] = new boolean[5];
+	private boolean continuePlaying = true;
+	private boolean placementDone = true;
+	private boolean resetDiceResult = false;
+	private boolean onePlayerRule;
+	private int gameOver = 0;
+	private static int rollCounter;
 	private static int turn;
-	public boolean diceThrow[] = new boolean[5];
+	
 	public static int[] diceResult = new int[5];
-	//Boolean [] Controller 
-
-	//*yoshi
-		
+	
+	/**
+	 * This constructor control the game i.e names, dice throw, who's turn it is etc.
+	 * @param pN is a string array that holding all players names.
+	 */
 	public Model(String[] pN){
 		
-		//yoshi*
 		for(int i=0; i<19; i++){
 			for(int j=0; j<6; j++){
 				underlyingScoreboard[i][j] = -1;
@@ -55,7 +55,6 @@ public class Model {
 		for(int i=0; i<6; i++){
 			placedOrNot[18][i] = true;
 		}
-		//*yoshi
 		
 		this.playerName = pN;
 		this.numberPlayers = playerName.length;
@@ -82,17 +81,38 @@ public class Model {
 		return scoreName;
 	}
 	
-	//yoshi*
+	/**
+	 * This method returns a players name from a string array, given from a specific index number. 
+	 * @param i is the index of a players name
+	 * @return a string that is a players name.
+	 */
 	public String getPlayerName(int i){
 		return playerName[i];
 	}
 	
-	public  void setScore(int score, int i, int j){
+	/**
+	 * This method assigns a score on the scoreboard that is a two dimensional array.
+	 * @param score is an integer representing the score you want to set.
+	 * @param i is a index of the row.
+	 * @param j is a index of the column.
+	 */
+	public void setScore(int score, int i, int j){
 		underlyingScoreboard[i][j] = score;
 	}
-	public  int getScore(int i, int j){
+	
+	/**
+	 * This method returns a score from the scoreboard that is a two dimensional array.
+	 * @param i equals row index.
+	 * @param j equals column index.
+	 * @return an integer representing the score from the scoreboard
+	 */
+	public int getScore(int i, int j){
 		return underlyingScoreboard[i][j];
 	}
+	
+	/**
+	 * This method keeps track of a players rolls, (maximum 3 rolls).
+	 */
 	public  void rollCounter(){
 		rollCounter = rollCounter + 1;
 		if(rollCounter==4){
@@ -103,89 +123,184 @@ public class Model {
 			}
 		}
 	}
+	
+	/**
+	 * This method keeps track of a players turn.
+	 */
 	public void whosTurn(){
 		for(int i=0; i<6; i++){
 			placedOrNot[0][i] = false;
 		}
 		placedOrNot[0][turn] = true;
 	}
+	
+	/**
+	 * This method assigns next player in turn.
+	 */
 	public void setNextInTurn(){
 		turn = turn + 1;
 		if(turn == numberPlayers){
 			turn = 0;
 		}
 	}
+	
+	/**
+	 *  This method returns an integer (index) representing who's turn it is.
+	 * @return an integer representing who's turn it is.
+	 */
 	public int getTurn(){
 		return turn;
 	}
-	public void continuePlay(){
-		for(int i=0; i<19; i++){
-			if(placedOrNot[i][getTurn()]){
-				booleanCounter = booleanCounter + 1;
-			}
-		}
-	}
-	public void roll() {
-		Dice d = new Dice();
+	
+	/**
+	 * This method generates all the dice numbers, (five dice each randomly 1-6).
+	 * @see Dice class.
+	 */
+	public void rollDice() {
+		Dice dice = new Dice();
 		for(int i=0; i<5; i++){
 			if(diceThrow[i]){
-				diceResult[i] = d.value();
+				diceResult[i] = dice.value();
 			}
 		}
 	}
+	
+	/**
+	 * This method returns an integer array holding all the dice results.
+	 * @return an integer array holding all the dice results.
+	 */
 	public int[] getRollResult() {
 		return diceResult;
 	}
+	
+	/**
+	 * This method resets the roll counter to zero.
+	 */
 	public void setResetRollCounter(){
 		rollCounter = 0;
 	}
+	
+	/**
+	 * This method returns an integer of the roll counter representing how many rolls there has been.
+	 * @return an integer representing how many rolls there has been.
+	 */
 	public int getRollCounter() {
 		return rollCounter;
 	}
+	
+	/**
+	 * This method returns an integer representing how many players participating in the game.
+	 * @return an integer representing how many players participating in the game.
+	 */
 	public int getNumberPlayers(){
 		return numberPlayers;
 	}
+	
+	/**
+	 *  This method returns a underlying scoreboard (an integer two dimensional array) holding all the scores.
+	 * @return a underlying scoreboard (an integer two dimensional array) holding all the scores.
+	 */
 	public int[][] getUnderlyingScoreboard(){
 		return underlyingScoreboard;
 	}
+	
+	/**
+	 * This method assigns that a placement has been done on the scoreboard.
+	 * @param i is an integer equals row index.
+	 * @param j is an integer equals column index.
+	 */
 	public void setPlacedTrue(int i, int j){
 		placedOrNot[i][j] = true;
 	}
-	public void setDiceThrow(int i, boolean b){
-		diceThrow[i] = b;
-	}
-	public boolean getDiceThrow(int i){
-		return diceThrow[i];
-	}
+	
+	/**
+	 * This method determines if a placement has been done, on a given row and column index.
+	 * @param i is an integer equals row index.
+	 * @param j is an integer equals column index.
+	 * @return Boolean value, true if a placement has been done.
+	 */
 	public boolean getPlaced(int i, int j){
 		return placedOrNot[i][j];
 	}
+	
+	/**
+	 * This method determines which one of the dice you want to throw
+	 * @param i is an integer representing one the dice 1-5.
+	 * @param b is a boolean representing if you want to throw the dice, true if you want to throw.
+	 */
+	public void setDiceThrow(int i, boolean b){
+		diceThrow[i] = b;
+	}
+	
+	/**
+	 * This method returns a boolean of a dice representing if you want to throw it or save it.
+	 * @param i is an integer representing one the dice 1-5.
+	 * @return a boolean representing if you want to throw the dice or save it, true if you want to throw.
+	 */
+	public boolean getDiceThrow(int i){
+		return diceThrow[i];
+	}
+	
+	/**
+	 * This method keeps track on a players rolls (maximum 3 rolls).
+	 */
 	public void continuePlaying(){
 		if(getRollCounter() == 3){
 			continuePlaying = false;
 		}
 	}
+	
+	/**
+	 * This method assigns a boolean representing if a player is allowed to roll or has to make a placement. True if the player are allowed to roll.
+	 * @param b is a boolean, true if a player can continue play.
+	 */
 	public void setContinuePlaying(boolean b){
 		continuePlaying = b;
 	}
+	
+	/**
+	 * This method returns a boolean representing if a player is allowed to roll or has to make a placement.
+	 * @return a boolean, true if the player are allowed to roll.
+	 */
 	public boolean getContinuePlaying(){
 		return continuePlaying;
 	}
+	
+	/**
+	 * This method assigns if a placement has been done or not.
+	 * @param b is a boolean representing if a placement has been done or not, true if a placement has been done.
+	 */
 	public void setPlacementDone(boolean b){
 		placementDone = b;
 	}
+	
+	/**
+	 * This method returns a boolean representing if a placement has been done or not.
+	 * @return a boolean representing if a placement has been done or not. True if a placement has been done.
+	 */
 	public boolean getPlacementDone(){
 		return placementDone;
 	}
+	
+	/**
+	 * This method assigns to reset the dice.
+	 * @param b is a boolean representing if you want to reset the dice, true if you want.
+	 */
 	public void setResetDiceResult(boolean b){
 		resetDiceResult = b;
 	}
+	
+	/**
+	 * This method returns a boolean representing if the dice are going to reset or not.
+	 * @return a boolean representing if the dice are going to reset, true if they are going to reset.
+	 */
 	public boolean getResetDiceResult(){
 		return resetDiceResult;
 	}
-	public int getGameCompleted(){
-		return gameCompleted;
-	}
+	
+	/**
+	 * This method calculates the sum of Yatzy-point category ones to sixes and assigns it to the underlying scoreboard.
+	 */
 	public void bonusSum(){
 		for(int k=0; k<6; k++){
 			underlyingScoreboard[7][k] = 0;
@@ -198,9 +313,19 @@ public class Model {
 			}
 		}
 	}
+	
+	/**
+	 * This method returns the sum of Yatzy-point category ones to sixes from one player, given a specific index.
+	 * @param i is an integer representing a index of a player.
+	 * @return an integer representing the sum of Yatzy-point category ones to sixes from one player.
+	 */
 	public int getBonusSum(int i){
 		return underlyingScoreboard[7][i];
 	}
+	
+	/**
+	 * This method calculates every Yatzy-point category and assigns it to the underlying scoreboard.
+	 */
 	public void totalSum(){
 		for(int k=0; k<6; k++){
 			underlyingScoreboard[18][k] = 0;
@@ -220,22 +345,32 @@ public class Model {
 			}
 		}
 	}
+	
+	/**
+	 * This method keeps track if the game is over.
+	 * @return a boolean representing if the game is over, true if the game is over.
+	 */
 	public boolean gameComplete(){
 		for(int i=1; i<19; i++){
 			for(int j=0; j<numberPlayers; j++){
 				if(!placedOrNot[i][j]){
-					gameCompleted = gameCompleted + 1;
+					gameOver = gameOver + 1;
 				}
 			}
 		}
-		if(gameCompleted == 0){
+		if(gameOver == 0){
 			return true;
 		}
 		else{
-			gameCompleted = 0;
+			gameOver= 0;
 			return false;
 		}
 	}
+	
+	/**
+	 * This method returns an integer representing an index of a player with the highest points, the winner.
+	 * @return an integer representing an index of a player with the highest points, the winner.
+	 */
 	public int whoIsTheWinner(){
 		int winner = 0;
 		int winnerIndex = -1;
@@ -247,11 +382,20 @@ public class Model {
 		}
 		return winnerIndex;
 	}
+	
+	/**
+	 * This method is a rule, only applied if there is only one player playing the game. To prevent multiple placement of one dice throw result.
+	 * @param b is a boolean representing a state that has to be true if the player are going to be able to make a placement.
+	 */
 	public void setOnePlayerRule(boolean b){
 		onePlayerRule = b;
 	}
+	
+	/**
+	 * This method returns a boolean representing a rule, only applied if there is only one player playing the game. To prevent multiple placement of one dice throw result.
+	 * @return a boolean representing a state that has to be true if the player are going to be able to make a placement.
+	 */
 	public boolean getOnePlayerRule(){
 		return onePlayerRule;
 	}
-	//*yoshi
 }
